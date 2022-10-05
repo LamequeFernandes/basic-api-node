c3066eff78b98bd5223e2f5e5c74d8f9084aad8e
Lameque Fernandes <lamequesao@gmail.com>
2022-10-05 15:24:11 -0300

    put, delete e get de filme

--- node+express+mongoDB/src/controllers/filmesController.js
+++ node+express+mongoDB/src/controllers/filmesController.js
@@ -12,6 +12,16 @@ class FilmeController {
     });
   }
 
+  static listarFilme = (req, res) => {
+    filmeModel.findById(req.params.id, (err, filme) => {
+      if (err) {
+        res.status(404).send({ message: err.message });
+      } else {
+        res.status(200).json(filme);
+      }
+    });
+  }
+
   static cadastrarFilme = (req, res) => {
     const filme = new filmeModel(req.body);
     filme.save((err) => {
@@ -23,5 +33,29 @@ class FilmeController {
     });
   }
 
+  static atualizarFilme = (req, res) => {
+    const id = req.params.id;
+    filmeModel.findByIdAndUpdate(id, req.body, (err) => {
+      if (err) {
+        res.status(500).json({ message: err.message });
+      } else {
+        res.status(200).json(
+          { message: "Filme atualizado com sucesso!" }, 
+          req.body
+          );
+      }
+    });
+  }
+
+  static excluirFilme = (req, res) => {
+    const id = req.params.id;
+    filmeModel.findByIdAndDelete(id, (err) => {
+      if (err) {
+        res.status(500).json({ message: err.message });
+      } else {
+        res.status(200).json({ message: "Filme excluído com sucesso!" });
+      }
+    });
+  }
 
 } export default FilmeController
--- node+express+mongoDB/src/routes/filmesRoutes.js
+++ node+express+mongoDB/src/routes/filmesRoutes.js
@@ -4,6 +4,9 @@ import FilmeController from "./../controllers/filmesController.js";
 const routerFilme = express.Router();
 
 routerFilme.get("/filmes", FilmeController.listarFilmes);
+routerFilme.get("/filmes/:id", FilmeController.listarFilme);
 routerFilme.post("/filmes", FilmeController.cadastrarFilme);
+routerFilme.put("/filmes/:id", FilmeController.atualizarFilme);
+routerFilme.delete("/filmes/:id", FilmeController.excluirFilme);
 
 export default routerFilme;
